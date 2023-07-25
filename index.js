367d4173d7274f1d69e2cb61435f027e6bdfb239
Alise Cervantes <alise09@icloud.com>
2023-07-24 19:32:49 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express')
-const app = express()
+// Require needed modules.
+const express = require("express");
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+// Initialize the app object.
+const app = express();
 
-app.listen(3000)
+// Create a homepage route.
+app.get("/", function (req, res) {
+  // This gets sent to the client
+  // (your web browser most likely!)
+  res.send("Hello world");
+});
 
+// Create a second route.
+app.get("/second", function (req, res) {
+  res.send("My Second Page!");
+});
+
+// Listen for connections.
+app.listen(3000, function () {
+  console.log("I am awake!");
+});
